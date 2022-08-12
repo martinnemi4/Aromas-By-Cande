b6cf2f6857720b864ebdad1bac1b6e7a7edd2597
Martin Nemi <martinnemi4@gmail.com>
2022-08-11 21:18:54 -0300

    Modificacion del título y parrafos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aromas By Cande <br></br>Tienda Online
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ir a la tienda
         </a>
       </header>
     </div>
